1ee410a7d6bb9d3a5fc309b224e105952b7232b8
GildeshAbhay <147131617+GildeshAbhay@users.noreply.github.com>
2025-03-24 11:09:46 +0530

    Update app.py
    removed app is running message

--- app.py
+++ app.py
@@ -22,11 +22,6 @@ app.add_middleware(
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def root():
-    return {"message": "App is running"}
-
-
 # Initialize query processor
 query_processor = QueryProcessor(api_key=os.getenv('OPENAI_API_KEY'))
 
